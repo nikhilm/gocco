11f1ecf7bf090caca2b6a0e6d0dd8cda9132a24d
Nikhil Marathe <nsm.nikhil@gmail.com>
2012-04-06 09:54:07 +0530

    Add literate documentation

--- src/gocco/gocco.go
+++ src/gocco/gocco.go
@@ -1,35 +1,33 @@
-// **Docco** is a quick-and-dirty, hundred-line-long, literate-programming-style
-// documentation generator. It produces HTML
-// that displays your comments alongside your code. Comments are passed through
+// **Gocco** is a Go port of [Docco](http://jashkenas.github.com/docco/): the
+// original quick-and-dirty, hundred-line-long, literate-programming-style
+// documentation generator. It produces HTML that displays your comments
+// alongside your code. Comments are passed through
 // [Markdown](http://daringfireball.net/projects/markdown/syntax), and code is
-// passed through [Pygments](http://pygments.org/) syntax highlighting.
-// This page is the result of running Docco against its own source file.
+// passed through [Pygments](http://pygments.org/) syntax highlighting.  This
+// page is the result of running Gocco against its own source file.
 //
-// If you install Docco, you can run it from the command-line:
+// If you install Gocco, you can run it from the command-line:
 //
-// docco src/*.coffee
+// gocco src/*.go
 //
-// ...will generate an HTML documentation page for each of the named source files,
-// with a menu linking to the other pages, saving it into a `docs` folder.
+// ...will generate an HTML documentation page for each of the named source
+// files, with a menu linking to the other pages, saving it into a `docs`
+// folder.
 //
-// The [source for Docco](http://github.com/jashkenas/docco) is available on GitHub,
-// and released under the MIT license.
+// The [source for Gocco](http://github.com/nikhilm/gocco) is available on
+// GitHub, and released under the MIT license.
 //
-// To install Docco, first make sure you have [Node.js](http://nodejs.org/),
-// [Pygments](http://pygments.org/) (install the latest dev version of Pygments
-// from [its Mercurial repo](http://dev.pocoo.org/hg/pygments-main)), and
-// [CoffeeScript](http://coffeescript.org/). Then, with NPM:
+// To install Gocco, first make sure you have [Pygments](http://pygments.org/)
+// Then, with the go tool:
 //
-// sudo npm install -g docco
+//     go install github.com/nikhilm/gocco
 package main
 
 import (
-    "sort"
-    "text/template"
-    "strings"
 	"bytes"
 	"container/list"
 	"flag"
+	"github.com/russross/blackfriday"
 	"io"
 	"io/ioutil"
 	"log"
@@ -37,13 +35,19 @@ import (
 	"os/exec"
 	"path"
 	"regexp"
+	"sort"
+	"strings"
 	"sync"
-	"github.com/russross/blackfriday"
+	"text/template"
 )
 
-const highlightStart = "<div class=\"highlight\"><pre>"
-const highlightEnd = "</pre></div>"
+// ## Types
+// Due to Go's statically typed nature, what is passed around in object
+// literals in Docco, requires various structures
 
+// A `Section` captures a piece of documentation and code
+// Every time interleaving code is found between two comments
+// a new `Section` is created.
 type Section struct {
 	docsText []byte
 	codeText []byte
@@ -51,30 +55,62 @@ type Section struct {
 	CodeHTML []byte
 }
 
+// a `TemplateSection` is a section that can be passed
+// to Go's templating system, which expects strings.
 type TemplateSection struct {
-    DocsHTML string
-    CodeHTML string
-    Index int
+	DocsHTML string
+	CodeHTML string
+    // The `Index` field is used to create anchors to sections
+	Index    int
 }
 
+// a `Language` describes a programming language
 type Language struct {
+	// the `Pygments` name of the language
 	name           string
+	// The comment delimiter
 	symbol         string
+	// The regular expression to match the comment delimiter
 	commentMatcher *regexp.Regexp
+	// Used as a placeholder so we can parse back Pygments output
+	// and put the sections together
 	dividerText    string
+	// The HTML equivalent
 	dividerHTML    *regexp.Regexp
 }
 
+// a `TemplateData` is per-file
 type TemplateData struct {
-    Title string
-    Sections []*TemplateSection
-    Sources []string
-    Multiple bool
+	// Title of the HTML output
+	Title    string
+	// The Sections making up this file
+	Sections []*TemplateSection
+	// A full list of source files so that a table-of-contents can
+	// be generated
+	Sources  []string
+	// Only generate the TOC is there is more than one file
+	// Go's templating system does not allow expressions in the
+	// template, so calculate it outside
+	Multiple bool
 }
 
+// a map of all the languages we know
 var languages map[string]*Language
+
+// paths of all the source files, sorted
 var sources []string
 
+// Wrap the code in these
+const highlightStart = "<div class=\"highlight\"><pre>"
+const highlightEnd = "</pre></div>"
+
+// ### Main documentation generation functions
+
+// Generate the documentation for a single source file
+// by splitting it into sections, highlighting each section
+// and putting it together.
+// The WaitGroup is used to signal we are done, so that the main
+// goroutine waits for all the sub goroutines
 func generateDocumentation(source string, wg *sync.WaitGroup) {
 	code, err := ioutil.ReadFile(source)
 	if err != nil {
@@ -86,6 +122,7 @@ func generateDocumentation(source string, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
+// Parse splits code into `Section`s
 func parse(source string, code []byte) *list.List {
 	lines := bytes.Split(code, []byte("\n"))
 	sections := new(list.List)
@@ -96,16 +133,24 @@ func parse(source string, code []byte) *list.List {
 	var codeText = new(bytes.Buffer)
 	var docsText = new(bytes.Buffer)
 
+    // save a new section
 	save := func(docs, code []byte) {
-        docsCopy, codeCopy := make([]byte, len(docs)), make([]byte, len(code))
-        copy(docsCopy, docs)
-        copy(codeCopy, code)
+		// deep copy the slices since slices always refer to the same storage
+		// by default
+		docsCopy, codeCopy := make([]byte, len(docs)), make([]byte, len(code))
+		copy(docsCopy, docs)
+		copy(codeCopy, code)
 		sections.PushBack(&Section{docsCopy, codeCopy, nil, nil})
 	}
 
 	for _, line := range lines {
+		// if the line is a comment
 		if language.commentMatcher.Match(line) {
+			// but there was previous code
 			if hasCode {
+				// we need to save the existing documentation and text
+				// as a section and start a new section since code blocks
+				// have to be delimited before being sent to Pygments
 				save(docsText.Bytes(), codeText.Bytes())
 				hasCode = false
 				codeText.Reset()
@@ -119,15 +164,22 @@ func parse(source string, code []byte) *list.List {
 			codeText.WriteString("\n")
 		}
 	}
+	// save any remaining parts of the source file
 	save(docsText.Bytes(), codeText.Bytes())
 	return sections
 }
 
+// `highlight` pipes the source to Pygments, section by section
+// delimited by dividerText, then reads back the highlighted output,
+// searches for the delimiters and extracts the HTML version of the code
+// and documentation for each `Section`
 func highlight(source string, sections *list.List) {
 	language := getLanguage(source)
 	pygments := exec.Command("pygmentize", "-l", language.name, "-f", "html", "-O", "encoding=utf-8")
 	pygmentsInput, _ := pygments.StdinPipe()
-    pygmentsOutput, _ := pygments.StdoutPipe()
+	pygmentsOutput, _ := pygments.StdoutPipe()
+	// start the process before we start piping data to it
+	// otherwise the pipe may block
 	pygments.Start()
 	for e := sections.Front(); e != nil; e = e.Next() {
 		pygmentsInput.Write(e.Value.(*Section).codeText)
@@ -182,6 +234,8 @@ func generateHTML(source string, sections *list.List) {
 }
 
 func goccoTemplate(data TemplateData) []byte {
+	// this hack is required because `ParseFiles` doesn't
+	// seem to work properly, always complaining about empty templates
 	r, x := ioutil.ReadFile("resources/gocco.got")
 	if x != nil {
 		panic(x)
@@ -190,6 +244,7 @@ func goccoTemplate(data TemplateData) []byte {
 	b.Write(r)
 
 	t, err := template.New("gocco").Funcs(
+	    // introduce the two functions that the template needs
 		template.FuncMap{
 			"base":        path.Base,
 			"destination": destination,
@@ -205,27 +260,36 @@ func goccoTemplate(data TemplateData) []byte {
 	return buf.Bytes()
 }
 
+// get a `Language` given a path
 func getLanguage(source string) *Language {
 	return languages[path.Ext(source)]
 }
 
+// make sure `docs/` exists
 func ensureDirectory(name string) {
 	os.MkdirAll(name, 0755)
 }
 
 func setupLanguages() {
 	languages = make(map[string]*Language)
+	// you should add more languages here
+	// only the first two fields should change, the rest should
+	// be `nil, "", nil`
 	languages[".go"] = &Language{"go", "//", nil, "", nil}
 }
 
 func setup() {
     setupLanguages()
+
+    // create the regular expressions based on the language comment symbol
 	for _, lang := range languages {
 		lang.commentMatcher, _ = regexp.Compile("^\\s*" + lang.symbol + "\\s?")
 		lang.dividerText = "\n" + lang.symbol + "DIVIDER\n"
 		lang.dividerHTML, _ = regexp.Compile("\\n*<span class=\"c1?\">" + lang.symbol + "DIVIDER<\\/span>\\n*")
 	}
 }
+
+// let's Go!
 func main() {
 	setup()
 
